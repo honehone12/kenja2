06816cae9f3f5c16e21922bc881a70481be49a95
honehone <honehone.41games@gmail.com>
2025-06-05 17:05:22 +0900

    fixed name

--- mongodb/atlas.go
+++ mongodb/atlas.go
@@ -6,13 +6,13 @@ import (
 	"kenja2/marshalers"
 )
 
-type AtlaSearch struct {
+type Atlas struct {
 	mongoClient
 	collections collections
 	marshaler   marshalers.Marshaler
 }
 
-func Connet[M marshalers.Marshaler](uri string, marshaler M) (*AtlaSearch, error) {
+func Connet[M marshalers.Marshaler](uri string, marshaler M) (*Atlas, error) {
 	mongoClient, err := connect(uri)
 	if err != nil {
 		return nil, err
@@ -23,17 +23,17 @@ func Connet[M marshalers.Marshaler](uri string, marshaler M) (*AtlaSearch, error
 		return nil, err
 	}
 
-	return &AtlaSearch{
+	return &Atlas{
 		mongoClient,
 		collections,
 		marshaler,
 	}, nil
 }
 
-func (a *AtlaSearch) TextSearch(keywords string, rating documents.Rating) {
+func (a *Atlas) TextSearch(keywords string, rating documents.Rating) {
 
 }
 
-func (a *AtlaSearch) Close(ctx context.Context) error {
+func (a *Atlas) Close(ctx context.Context) error {
 	return a.client.Disconnect(ctx)
 }
