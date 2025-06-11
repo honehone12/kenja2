4f8bf70021c6b369faea3844201f722b5644d4b1
honehone <honehone.41games@gmail.com>
2025-06-11 16:28:30 +0900

    added handlers

--- lambda/lib.go
+++ lambda/lib.go
@@ -1,3 +1,12 @@
 package lambda
 
+import "errors"
+
 const REQUEST_BODY_LIMIT = 1024
+
+func CheckBodyLimit(req []byte) error {
+	if len(req) > REQUEST_BODY_LIMIT {
+		return errors.New("request body size over limit")
+	}
+	return nil
+}
--- lambda/text/main.go
+++ lambda/text/main.go
@@ -5,6 +5,7 @@ import (
 	"kenja2"
 	"kenja2/endec"
 	"kenja2/engine"
+	kenja2lambda "kenja2/lambda"
 	"kenja2/lambda/logs"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,11 +13,13 @@ import (
 
 var __ENGINE engine.Engine[endec.Json, endec.Json]
 
-func handler(
-	ctx context.Context,
-	req []byte,
-) ([]byte, error) {
+func handler(ctx context.Context, req []byte) ([]byte, error) {
+	if err := kenja2lambda.CheckBodyLimit(req); err != nil {
+		logs.Error(err)
+		return nil, err
+	}
 
+	return __ENGINE.TextSearch(ctx, req)
 }
 
 func main() {
